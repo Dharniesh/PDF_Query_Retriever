8875d6b7db3435c6c22838f02843dd9ff89185e7
Dharniesh P R <70947728+Dharniesh@users.noreply.github.com>
2023-07-27 16:24:25 +0530

    Update testchatgpt.py

--- testchatgpt.py
+++ testchatgpt.py
@@ -43,11 +43,11 @@ def query_find(query):
 
     # Assuming 'docs' is the list containing your documents or elements
     #print(docs)
-    if docs[0].metadata['page']:
+    if 'page' in docs[0].metadata:
         pg_ref = [doc.metadata['page'] + 1 for doc in docs]
     else: 
         pg_ref =  None
-    if docs[0].metadata['source']:  
+    if 'source' in docs[0].metadata:  
         doc_name = [doc.metadata['source'] for doc in docs]
     else:
         doc_name=None
